4ab9be00cbf7c4c126886e4d788e155d5be76af1
Tanvi Bhakta <tanvibhakta@gmail.com>
2024-08-03 21:18:13 +0530

    create store

---
+++ src/app/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit'
+import postsReducer from '@/features/posts/postsSlice'
+
+export const store = configureStore({
+  reducer: {
+  }
+})
+
+
+export type AppStore = typeof store
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
